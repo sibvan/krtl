a28b122a06f2ebd15b4ba5ab142db6a3f2e856f6
sibvan <sibvan.dev@gmail.com>
2025-08-30 18:30:49 +0400

    add footer

--- src/assets/components/Footer.vue
+++ src/assets/components/Footer.vue
@@ -1,3 +1,5 @@
 <template>
-
+  <footer class="footer">
+    <p class="footer__text">Created by <a class="footer__link" target="_blank" href="http://sibvan.dev">sibvan.dev</a></p>
+  </footer>
 </template>
